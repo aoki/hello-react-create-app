8d73565bf7c8062796f8e65d17a745fe9922e8b5
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-11-20 00:17:23 +0900

    Hello React

---
+++ todo/src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../components/Home';
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Hello !!</h1>
+        <Home />
+      </div>
+    );
+  }
+});
--- todo/src/index.js
+++ todo/src/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-
-
-// Reducer
-const counterReducer = (state = {count: 0}, action) => {
-  const count = state.count;
-  switch (action.type) {
-    case 'ACTION_INCREMENT_COUNTER':
-      console.log('ACTION: INCREMENT_COUNTR');
-      return {count: count + action.count};
-    case 'ACTION_DECREMENT_COUNTER':
-      console.log('ACTION: DECRIMENT_COUNTR');
-      return {count: count + action.count};
-    default:
-      return
-  }
-}
-
-
-// Main ------------------------------
-import { createStore } from 'react';
-const store = createStore(counterReducer);
+import App from './containers/App';
 
 ReactDOM.render(
-  <Privider store="{store}">
-    <App />
-  </Privider>,
+  <App />,
   document.getElementById('root')
 );
